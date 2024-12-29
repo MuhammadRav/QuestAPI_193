1ad34aee3a74071e3d0b485be0d204629e80862d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-29 15:05:21 +0700

    menambahkan tampilan status untuk HomeScreen (loading, error, data mahasiswa)

--- app/src/main/java/com/example/meet12/view/HomeView.kt
+++ app/src/main/java/com/example/meet12/view/HomeView.kt
@@ -1,6 +1,11 @@
 package com.example.meet12.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,14 +13,21 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet12.CostumeTopAppBar
+import com.example.meet12.R
+import com.example.meet12.model.Mahasiswa
 import com.example.meet12.navigasi.DestinasiNavigasi
+import com.example.meet12.viewModel.HomeUiState
 import com.example.meet12.viewModel.HomeViewModel
 
 object DestinasiHome : DestinasiNavigasi{
@@ -61,11 +73,56 @@ fun HomeScreen(
             retryAction = { viewModel.getMhs()},
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick,
-            onDetailClick = {
+            onDeleteClick = {
                 viewModel.deleteMhs(it.nim)
                 viewModel.getMhs()
             }
         )
     }
 }
+
+@Composable
+fun HomeStatus(
+  homeUiState: HomeUiState,
+  retryAction: () -> Unit,
+  modifier: Modifier = Modifier,
+  onDeleteClick: (Mahasiswa) -> Unit = {},
+  onDetailClick: (String) -> Unit
+){
+    when(homeUiState){
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.mahasiswa.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center){
+                    Text("Tidak ada data mahasiswa")
+                }
+            }else{
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.nim)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(
+  modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
 
